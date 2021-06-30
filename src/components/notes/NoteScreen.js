e73f8da6cdecd7c23052f6380bccff5c4a4f8e17
Jimmy404 <bruno_pier@hotmail.com>
2021-06-30 19:53:06 -0300

    Adds container for NoteScreen

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -6,6 +6,27 @@ export const NoteScreen = () => {
   return (
     <div className="notes__main-content">
       <NotesAppBar />
+      <div className="notes__content">
+        <input
+          type="text"
+          placeholder="Some awesome title"
+          className="notes__title-input"
+          autoComplete="off"
+        />
+        <textarea
+          placeholder="Whats happened today?"
+          className="notes__textarea"
+        >
+        </textarea>
+        <div
+          className="notes__image"
+        >
+          <img
+            src="https://images.freeimages.com/images/large-previews/0b3/burning-tree-1377053.jpg"
+            alt="Heaven sun"
+          />
+        </div>
+      </div>
     </div>
   )
 }

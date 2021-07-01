7c0ee6fc58ae5542cddc2fa1e05e4a62e2408e91
Jimmy404 <bruno_pier@hotmail.com>
2021-07-01 18:11:23 -0300

    Adds dispatch actions for auth with Google

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-import { login } from '../../actions/auth';
+import { login, startGoogleLogin, startLoginEmailPassword } from '../../actions/auth';
 
 import { useForm } from '../../hooks/useForm';
 
@@ -19,7 +19,11 @@ export const LoginScreen = () => {
 
   const handleLogin = (e) => {
     e.preventDefault();
-    dispatch(login(12345,'Jimmy')); // Test
+    dispatch(startLoginEmailPassword(email, password)); // Test async
+  }
+
+  const handleGoogleLogin = () => {
+    dispatch(startGoogleLogin());
   }
 
   return (
@@ -55,6 +59,7 @@ export const LoginScreen = () => {
           <p>Login with social networks</p>
           <div
             className="google-btn"
+            onClick={handleGoogleLogin}
           >
             <div className="google-icon-wrapper">
               <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />

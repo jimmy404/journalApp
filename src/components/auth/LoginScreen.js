080fd9d823da92510fa5a230f74d236c2e5d356c
Jimmy404 <bruno_pier@hotmail.com>
2021-07-01 17:05:25 -0300

    Adds custom hook and dispatch action for login

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,16 +1,38 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+import { login } from '../../actions/auth';
+
+import { useForm } from '../../hooks/useForm';
+
 export const LoginScreen = () => {
+
+  const dispatch = useDispatch()
+
+  const [ formValues, handleInputChange] = useForm({
+    email: '',
+    password: ''
+  });
+
+  const { email, password } = formValues;
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    dispatch(login(12345,'Jimmy')); // Test
+  }
+
   return (
     <>
       <h3 className="auth__title">Login</h3>
-      <form>
+      <form onSubmit={handleLogin}>
         <input
         className="auth__input"
           type="text"
           placeholder="Email"
           name="email"
+          value={email}
+          onChange={handleInputChange}
         />
 
         <input
@@ -19,6 +41,8 @@ export const LoginScreen = () => {
           type="password"
           placeholder="Password"
           name="password"
+          value={password}
+          onChange={handleInputChange}
         />
 
         <button

9e4e5f6bd9b5e92efe9929a8d11c9baee57b40fb
Jimmy404 <bruno_pier@hotmail.com>
2021-07-01 18:07:57 -0300

    Adds auth with Google

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,5 +1,24 @@
 import { types } from '../types/types';
 
+import { firebase, googleAuthProvider } from '../firebase/firebase-config';
+
+export const startLoginEmailPassword = (email, password) => {
+  return (dispatch) => {
+    setTimeout(() => {
+      dispatch(login(12345,'asyncTest'))
+    }, 2000);
+  }
+}
+
+export const startGoogleLogin = () => {
+  return (dispatch) => {
+    firebase.auth().signInWithPopup(googleAuthProvider)
+      .then(({user}) => {
+        dispatch(login(user.uid, user.displayName))
+      })
+  }
+}
+
 export const login = (uid, displayName) => ({
   type: types.login,
   payload: {

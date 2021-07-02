5299c17daf444f31e7147be9e7ad051429cb16fd
Jimmy404 <bruno_pier@hotmail.com>
2021-07-02 13:07:40 -0300

    Adds useForm to handle user form

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from '../../hooks/useForm';
 
 export const RegisterScreen = () => {
+
+  const [ formValues, handleInputChange ] = useForm({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+
+  const { name, email, password, password2 } = formValues;
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+  }
+
+  const isFormValid = () => {
+
+  }
+
   return (
     <>
       <h3 className="auth__title">Register</h3>
-      <form>
+      <form onSubmit={handleRegister}>
         <input
           autoComplete="off"
           className="auth__input"
           name="name"
           placeholder="Name"
           type="text"
+          value={name}
+          onChange={handleInputChange}
         />
 
         <input
@@ -20,22 +41,26 @@ export const RegisterScreen = () => {
           name="email"
           placeholder="Email"
           type="text"
+          value={email}
+          onChange={handleInputChange}
         />
 
         <input
-          autoComplete="off"
           className="auth__input"
           name="password"
           placeholder="Password"
           type="password"
+          value={password}
+          onChange={handleInputChange}
         />
 
         <input
-          autoComplete="off"
           className="auth__input"
-          name="confirm"
+          name="password2"
           placeholder="Repeat Password"
           type="password"
+          value={password2}
+          onChange={handleInputChange}
         />
 
         <button

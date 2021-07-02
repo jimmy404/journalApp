1dcc8dd3aaef5b540e8373eb2ad9b5962bf7eb47
Jimmy404 <bruno_pier@hotmail.com>
2021-07-02 13:22:42 -0300

    Adds validations for form and TEST for alert message

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import validator from 'validator';
+
 import { useForm } from '../../hooks/useForm';
 
 export const RegisterScreen = () => {
@@ -15,16 +17,30 @@ export const RegisterScreen = () => {
 
   const handleRegister = (e) => {
     e.preventDefault();
+    if ( isFormValid() ) {
+      console.log('Form ok')
+    }
   }
 
   const isFormValid = () => {
-
+    if ( name.trim().length === 0 ){
+      console.log('Name is required');
+      return false;
+    } else if ( !validator.isEmail(email) ) {
+      console.log('Email is not valid');
+      return false;
+    } else if ( password !== password2 || password.length < 5 ) {
+      console.log('Password should be at least 6 characters and match each other');
+      return false;
+    }
+    return true;
   }
 
   return (
     <>
       <h3 className="auth__title">Register</h3>
       <form onSubmit={handleRegister}>
+        <div className="auth__alert-error">Alert messagge TEST</div>
         <input
           autoComplete="off"
           className="auth__input"

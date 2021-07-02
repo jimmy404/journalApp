ab9f8150710875b489c71a988dada73a802dc234
Jimmy404 <bruno_pier@hotmail.com>
2021-07-02 17:03:46 -0300

    Implements reducer for loading

--- src/reducers/uiReducer.js
+++ src/reducers/uiReducer.js
@@ -13,11 +13,23 @@ export const uiReducer = (state = initialState, action) => {
         msgError: action.payload
       }
 
-      case types.uiRemoveError:
-      return {
-        ...state,
-        msgError: null
-      }
+    case types.uiRemoveError:
+    return {
+      ...state,
+      msgError: null
+    }
+
+    case types.uiStartLoading:
+    return {
+      ...state,
+      loading: true
+    }
+
+    case types.uiFinishLoading:
+    return {
+      ...state,
+      loading: false
+    }
 
     default:
       return state;

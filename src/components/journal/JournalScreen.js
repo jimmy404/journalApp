9b8f30306f9e67c66501aa54588882baf3082b7d
Jimmy404 <bruno_pier@hotmail.com>
2021-07-03 13:40:09 -0300

    Implements active notes directly from state

--- src/components/journal/JournalScreen.js
+++ src/components/journal/JournalScreen.js
@@ -1,16 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
 
-// import { NothingSelected } from './NothingSelected';
+import { NothingSelected } from './NothingSelected';
 import { Sidebar } from './Sidebar';
 import { NoteScreen } from '../notes/NoteScreen';
 
 export const JournalScreen = () => {
+
+  const { active } = useSelector(state => state.notes);
+
   return (
     <div className="journal__main-content">
       <Sidebar />
       <main>
-        {/* <NothingSelected /> */}
-        <NoteScreen />
+        {
+          (active)
+          ? (<NoteScreen />)
+          : (<NothingSelected />)
+        }
       </main>
     </div>
   )

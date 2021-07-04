1f0b80e066d30504b77c453bd6f8ddd5f75c8b2b
Jimmy404 <bruno_pier@hotmail.com>
2021-07-04 14:14:39 -0300

    Implements handlers for image upload

--- src/components/notes/NotesAppBar.js
+++ src/components/notes/NotesAppBar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { startSaveNote } from '../../actions/notes';
+
+import { startSaveNote, startUploading } from '../../actions/notes';
 
 export const NotesAppBar = () => {
 
@@ -12,11 +13,32 @@ export const NotesAppBar = () => {
     dispatch(startSaveNote(active));
   }
 
+  const handlePictureClick = () => {
+    document.querySelector('#fileSelector').click();
+  }
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      dispatch(startUploading(file));
+    }
+  }
+
   return (
     <div className="notes__appbar">
       <span>30 Junio 2021</span>
+      <input
+        id="fileSelector"
+        type="file"
+        name="file"
+        style={{display: 'none'}}
+        onChange={handleFileChange}
+      />
       <div>
-        <button className="btn">
+        <button
+          className="btn"
+          onClick={handlePictureClick}
+        >
           Picture
         </button>
         <button
